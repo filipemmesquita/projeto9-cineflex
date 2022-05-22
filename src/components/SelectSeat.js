0ce7d5d3458491c33ef410655039abdcf245d269
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-05-22 08:35:32 -0300

    seleção e desseleção de assentos implementado(ainda falta o envio)  Changes to be committed: 	modified:   src/components/SelectSeat.js

--- src/components/SelectSeat.js
+++ src/components/SelectSeat.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 
 export default function SelectSeat(){
     const params = useParams();
-    const [SessionInfo, setSessionInfo]=useState(null);
+    const [selectedSeats, setSelectedSeats] = useState([]);
+    const [sessionInfo, setSessionInfo]=useState(null);
     useEffect(() => {
         const requisition=axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${params.idSessao}/seats`)
         requisition.then(response=>{
@@ -13,34 +14,71 @@ export default function SelectSeat(){
             setSessionInfo(response.data)
         });
     }, []);
-    if(SessionInfo===null){
+
+    function addSeat(id){
+        const newSelectedSeats=[...selectedSeats, id];
+        setSelectedSeats(newSelectedSeats);
+    };
+    function removeSeat(id){
+        const newSelectedSeats=[...selectedSeats];
+        setSelectedSeats( newSelectedSeats.filter(currentSeat => currentSeat!==id));
+    };
+
+    if(sessionInfo===null){
         return (<h1>carregando</h1>);
-    }
+    };
     return(
         <>
             <h1>Selecione o(s) assento(s)</h1>
-            <SeatSelection>
-                {SessionInfo.seats.map(seat=><Seat isAvailable={seat.isAvailable} id={seat.id}>{seat.name}</Seat>)}
-            </SeatSelection>
+            <SeatSelectionWrapper>
+                {sessionInfo.seats.map(seat=><Seat isAvailable={seat.isAvailable} id={seat.id} name={seat.name} addSeat={addSeat} removeSeat={removeSeat} />)}
+            </SeatSelectionWrapper>
         </>
     );
 
 }
+function Seat(props){
+    const [isSelected, setIsSelected]=useState(false);
+    function notAvailable(){
+        alert("Este assento não está disponível")
+    }
+    function seatSelection(){
+        if(isSelected){
+            setIsSelected(false)
+            props.removeSeat(props.id);
+        };
+        if(!isSelected){
+            setIsSelected(true)
+            props.addSeat(props.id);
 
-const SeatSelection = styled.div`
+        };
+    }
+    return(
+        <SeatButton
+            isAvailable={props.isAvailable}
+            isSelected={isSelected} 
+            id={props.id}
+            onClick={props.isAvailable ? seatSelection : notAvailable }
+        >
+            {props.name}
+        </SeatButton>
+    );
+}
+
+const SeatSelectionWrapper = styled.div`
 margin: 0 auto;
 display: flex;
 flex-wrap: wrap;
 width:320px;
 justify-content: center;
 `;
-const Seat = styled.div`
+const SeatButton = styled.button`
 box-sizing: border-box;
 margin:0 3px 26px;
 width:26px;
 height: 26px;
-background: ${props => props.isAvailable ? "#C3CFD9" : "#FBE192"} ;
-border: 1px solid ${props => props.isAvailable ? "#808F9D" : "#F7C52B"} ;
+background: ${props => props.isAvailable ? props.isSelected ? "#8DD7CF" : "#C3CFD9"  : "#FBE192"} ;
+border: 1px solid ${props => props.isAvailable ? props.isSelected ? "#45BDB0" : "#808F9D" : "#F7C52B"} ;
 border-radius: 12px;
 font-family: 'Roboto';
 font-style: normal;
